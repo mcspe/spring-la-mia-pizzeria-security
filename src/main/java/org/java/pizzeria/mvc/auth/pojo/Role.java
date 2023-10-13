8f67963a53c6dfb6934fbf9526698c9b8211c568
mcspe <mcspezzaferro@gmail.com>
2023-10-13 17:14:04 +0200

    [add] user and role entity classes

---
+++ src/main/java/org/java/pizzeria/mvc/auth/pojo/Role.java
@@ -0,0 +1,54 @@
+package org.java.pizzeria.mvc.auth.pojo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(nullable = false, unique = true)
+	@NotBlank
+	private String name;
+	
+	public Role() { }
+	public Role(String name) {
+		setName(name);
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "[" + getId() + "] " + getName();
+	}
+	@Override
+	public int hashCode() {
+		return getId();
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Role)) return false;
+		Role objRole = (Role) obj;
+		return getId() == objRole.getId();
+	}
+	
+}
