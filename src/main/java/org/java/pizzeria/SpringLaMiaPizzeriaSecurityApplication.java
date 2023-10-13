7bf980bfb4164ee5c20e5f6528fe87085ad2646e
mcspe <mcspezzaferro@gmail.com>
2023-10-13 16:29:33 +0200

    [set repo]

---
+++ src/main/java/org/java/pizzeria/SpringLaMiaPizzeriaSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.java.pizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLaMiaPizzeriaSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLaMiaPizzeriaSecurityApplication.class, args);
+	}
+
+}
